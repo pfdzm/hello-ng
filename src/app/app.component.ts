d825da6ba50f928fd9144b20610155133d57e768
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-31 22:42:25 +0200

    first steps into angular!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>
+      {{ title }}
+    </h1>
+  `,
+  styles: [
+    `
+      h1 {
+        color: palevioletred;
+        font-family: 'Segoe UI', sans-serif;
+        font-weight: 900;
+        font-size: xx-large;
+      }
+    `
+  ]
 })
 export class AppComponent {
   title = 'egghead-angular';
